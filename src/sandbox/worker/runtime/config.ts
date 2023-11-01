c19494663cad12737f4d10d14d2620533dbcf92f
kiootic <kiootic@gmail.com>
2023-11-02 01:42:18 +0800

    Fix config type

--- src/sandbox/worker/runtime/config.ts
+++ src/sandbox/worker/runtime/config.ts
@@ -1,8 +1,8 @@
 export class ConfigStore<T> {
-  private readonly values = new Map<keyof T, unknown>();
-  private readonly callbacks = new Map<keyof T, ((value: unknown) => void)[]>();
+  private readonly values = new Map<string, unknown>();
+  private readonly callbacks = new Map<string, ((value: unknown) => void)[]>();
 
-  define<K extends keyof T>(name: K, value: T[K]): T[K] {
+  define<K extends string, T>(name: K, value: T): T & { __name?: K } {
     if (this.values.has(name)) {
       throw new TypeError(`Duplicated config ${String(name)}`);
     }
@@ -12,17 +12,18 @@ export class ConfigStore<T> {
     if (callbacks != null) {
       callbacks.forEach((cb) => cb(value));
     }
-    return value;
+    return value as T & { __name?: K };
   }
 
-  configure<K extends keyof T>(name: K, fn: (value: T[K]) => void) {
-    if (this.values.has(name)) {
-      const value = this.values.get(name) as T[K];
+  configure<K extends keyof T>(name: keyof T, fn: (value: T[K]) => void) {
+    const key = name as string;
+    if (this.values.has(key)) {
+      const value = this.values.get(key) as T[K];
       fn(value);
       return;
     }
 
-    const callbacks = this.callbacks.get(name) ?? [];
-    this.callbacks.set(name, [...callbacks, fn as (value: unknown) => void]);
+    const callbacks = this.callbacks.get(key) ?? [];
+    this.callbacks.set(key, [...callbacks, fn as (value: unknown) => void]);
   }
 }
